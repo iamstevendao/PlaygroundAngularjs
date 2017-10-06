0a6afbd1d51a093af5f75742c07d6199a83b1e41
Steven Dao <iamstevendao@gmail.com>
2017-10-06 14:14:42 +1100

    convertCurrency

--- invoice.js
+++ invoice.js
@@ -5,9 +5,14 @@
     this.cost = 2;
     this.inCurr = 'AUD';
     this.currencies = ['USD', 'EUR', 'AUD'];
+    this.usdToForeignRates = {USD: 10, EUR: 0.74, AUD: 1}
 
-    this.total = function total() {
-      return this.qty * this.cost;
+    this.total = function(outCurr) {
+      return this.convertCurrency(this.qty * this.cost, this.inCurr, outCurr);
+    }
+
+    this.convertCurrency = function(amount, inCurr, outCurr) {
+      return amount * this.usdToForeignRates[inCurr] / this.usdToForeignRates[outCurr];
     }
 
   });
